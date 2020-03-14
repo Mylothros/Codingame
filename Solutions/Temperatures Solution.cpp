5e4c34fac280494bf6e0541dbe7d50d8cd9c96f8
Mylothros <mbabis1995@hotmail.com>
2020-03-14 18:11:45 +0200

    Update Temperatures Solution.cpp

--- Solutions/Temperatures Solution.cpp
+++ Solutions/Temperatures Solution.cpp
@@ -1,58 +1,40 @@
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
 
 using namespace std;
 
 int main()
 {
-    int n,result,temp1,temp2,min; // the number of temperatures to analyse
+    int n;    
     cin >> n; cin.ignore();
-    for (int i = 0; i < n; i++) {
-        int t; // a temperature expressed as an integer ranging from -273 to 5526
+    int a[n],res;
+    
+    for (int i = 0; i < n; i++)
+    {
+        int t;
         cin >> t; cin.ignore();
-        if (n == 1) //no temperatures are added
+        a[i]=t;
+    }
+    
+    if(n==0)
+    {
+        res=0;    
+    }
+    else
+    {
+        sort(a, a + n);//sorting the array
+        for(int i = 0; i < n; i++)
         {
-            result=t;    
-        }
-        if (i == 0)
-        {
-            min=t;//setting the first temperature to be the min
-        }
-        else if (i >= 1)
-        {
-            if (abs(min) > abs(t)) //checking if the absolute number of the new temp is the new min
+            if(a[i] > 0)//finding the first temperature closest to 0
             {
-                min=t;
-                result=min;
+                res = a[i];
+                break;
             }
-            if (min > t) //checking iif the new temp is the new min
+            if(i == n-1)//if last loop and line 31 never was true then closest temperature is the last negative temperature n-1
             {
-                min=min;
-                result=min;
-            }
-            
-            if (abs(min) == abs(t)) //checking if the absolute number of the 2 temps is equal
-            {
-                temp1 = min+min;
-                temp2 = t+t;
-                //finding which is +
-                if(temp1>0)
-                {         
-                    result=min;    
-                }
-                else if(temp2>0)
-                {
-                    result=t;    
-                }
-                if (temp1<0 && temp2<0) //if neither is + then result=min
-                {
-                    result=min;
-                }
-                
+                res = a[i];    
             }
         }
     }
-    cout << result << endl;
+    cout << res << endl;
 }
