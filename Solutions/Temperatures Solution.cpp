4a4bddf60e614c6b331bc8dec3ef6d2e98699dec
Mylothros <mbabis1995@hotmail.com>
2020-03-18 18:46:41 +0200

    Update Temperatures Solution.cpp

--- Solutions/Temperatures Solution.cpp
+++ Solutions/Temperatures Solution.cpp
@@ -7,32 +7,49 @@ int main()
 {
     int n;    
     cin >> n; cin.ignore();
-    int a[n],res;
-    
+    int a[n],res,res1=0,res2=0;
+    bool b=true;
     for (int i = 0; i < n; i++)
     {
         int t;
         cin >> t; cin.ignore();
-        a[i]=t;
+        a[i] = t;
     }
-    
-    if(n==0)
+   
+    if(n == 0)//if there are no temperatures
     {
-        res=0;    
+        res = 0; 
     }
     else
     {
         sort(a, a + n);//sorting the array
         for(int i = 0; i < n; i++)
         {
-            if(a[i] > 0)//finding the first temperature closest to 0
+            if(a[i] > 0)//finding the first positive temperature closest to 0
             {
-                res = a[i];
+                res1 = a[i];
+                res2 = a[i-1];
                 break;
             }
-            if(i == n-1)//if last loop and line 31 never was true then closest temperature is the last negative temperature n-1
+            else if(i == n-1)//if all a[i] < 0 then res should be the biggest negative number which is the nearest number to 0
             {
-                res = a[i];    
+                res =  a[i];
+                b = false;//dont check the requirements
+            }
+        }
+        if(b == true)//check the requirements
+        {
+            if(abs(res1) > abs(res2))
+            {
+                res = res2;
+            }
+            else if(abs(res1) < abs(res2))
+            {
+                res = res1;    
+            }
+            else if(abs(res1) == abs(res2))
+            {
+                res = abs(res1);   
             }
         }
     }
