8750debc6272ea903271da32fc5494aa7b9fff95
Mylothros <mbabis1995@hotmail.com>
2020-03-20 07:49:58 +0200

    Update Create the longest sequence of 1s Solution.cpp

--- Solutions/Create the longest sequence of 1s Solution.cpp
+++ Solutions/Create the longest sequence of 1s Solution.cpp
@@ -9,7 +9,7 @@ int main()
     getline(cin, b);   
     int counter[b.size() + 1] = {0},  holder[b.size() + 1], c=0;
     /*Counter is an array to count the '1' in the string and holder
-     is the array which holds all the '1' before and after a '0' (line 24). The length is b.size() + 1
+     is the array which holds the sum of all the '1' before and after a '0' (line 24). The length is b.size() + 1
      because all characters (numbers) could be 1. If all characters are '0' then the length should be +1 because
      if there are 2 '0' then the for in line 22 will run in range 0 to 1. So if i = 1 then counter[i + 1] will go out of bounds in line 24.
     */
